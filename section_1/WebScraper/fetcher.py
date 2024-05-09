8bd57b2d51dbb9e534aad94ad26c91b596743ef1
mateuszw-tech <mateuszw.tech@gmail.com>
2024-05-09 19:01:52 +0200

    Add get_all_offers_urls feature to fetcher.py

--- section_1/WebScraper/fetcher.py
+++ section_1/WebScraper/fetcher.py
@@ -1,5 +1,8 @@
+import concurrent
 from abc import ABC, abstractmethod
+from concurrent.futures import ThreadPoolExecutor
 from typing import List
+from utils import SprzedajemyUtils, ScraperUtils
 
 
 class AdvertisementInfo:
@@ -19,9 +22,22 @@ class Fetcher(ABC):
 
 
 class SprzedajemyFetcher(Fetcher):
-    def load_osint_data(self) -> List[AdvertisementInfo]:
 
+    def load_osint_data(self) -> List[AdvertisementInfo]:
         pass
 
+    # todo: add multiple cities argument
+    @staticmethod
+    def get_all_offers_urls(city: str, threads: int) -> List[str]:
+        # pages = ScraperUtils.get_all_pages_urls_from_different_cities(SprzedajemyUtils.get_all_pages_urls, cities)
+        pages = SprzedajemyUtils.get_all_pages_urls(city)
+        offer_urls_futures = []
+        results_list = []
+        with ThreadPoolExecutor(max_workers=threads) as executor:
+            for page in pages:
+                offer_urls_futures.append(executor.submit(SprzedajemyUtils.find_all_offers_in_selected_page, page))
 
-
+            completed_futures, _ = concurrent.futures.wait(offer_urls_futures)
+            for future in completed_futures:
+                results_list.append(future.result())
+        return results_list
